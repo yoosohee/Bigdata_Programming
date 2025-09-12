5916f05e01ca61888e01f763409fad8a3ad33b7f
yoosohee <rrwnsldj@naver.com>
2025-09-12 15:31:48 +0900

    ch02 실습

---
+++ ch02/sample02.py
@@ -0,0 +1,11 @@
+a=10
+print(a)
+print(type(a)) #변수 타입
+print(id(a)) #객체의 주소 반환
+print(dir(a)) #내부 함수 나열
+
+b='10'
+print(b)
+print(type(b))
+print(id(b))
+print(dir(b))
---
+++ ch02/sample05.py
@@ -0,0 +1,9 @@
+a=0.1
+b=0.2
+c=a+b
+print(c) #부동소수점 오차 (자릿수를 정하거나 아래처럼 Decimal타입 사용)
+
+a1=Decimal('0.1')
+b1=Decimal('0.2')
+c=a1+b1
+print(c)
