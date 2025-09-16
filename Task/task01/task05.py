5ad5ed0634f1dd1c47a8ea436551400e59ae8ed6
usag1 <144187256+yoosohee@users.noreply.github.com>
2025-09-16 19:55:14 +0900

    task05 파일 수정

--- Task/task01/task05.py
+++ Task/task01/task05.py
@@ -1,8 +1,9 @@
 #다음 코드의 실행 결과는?
 
-a = 100
-result = 0
-for i in range(1,3):
-   result = a >> i
-   result = result + 1
-print(result)
+def cs(n):
+  s = 0
+  for num in range(n + 1):
+    s += num
+  return s
+
+print(cs(11))
