2554e26ce5e3fc9807030e34c0a713dffdb50fc4
usag1 <144187256+yoosohee@users.noreply.github.com>
2025-09-19 08:51:56 +0900

    Update and rename task02.py to task01.py

---
+++ Task/task01/task01.py
@@ -0,0 +1,9 @@
+#다음 코드의 실행 결과는?
+
+lol = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+print(lol[0])
+print(lol[2][1])
+for sub in lol:
+    for item in sub:
+        print(item, end=' ')
+    print()
--- Task/task01/task02.py
+++
@@ -1,5 +0,0 @@
-#다음 코드의 실행 결과는?
-
-String = 'Conceptual Schema'
-r = String[-4:6:-2]
-print(r)
